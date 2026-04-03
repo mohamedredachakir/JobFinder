5f2f804dd5d1a3ad92decdfe51fa2cfce3d42259
reddcs <chakirm082@gmail.com>
2026-04-03 19:56:30 +0100

    feat: add backend - src - main - java - com - jobfinder - config - SwaggerConfig.java

---
+++ backend/src/main/java/com/jobfinder/config/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package com.jobfinder.config;
+
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.jobfinder.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .securityContexts(Collections.singletonList(securityContext()));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "JobFinder API",
+                "API REST JobFinder",
+                "1.0",
+                "https://jobfinder.local/terms",
+                new Contact("JobFinder Team", "https://jobfinder.local", "contact@jobfinder.local"),
+                "MIT",
+                "https://opensource.org/licenses/MIT",
+                Collections.emptyList()
+        );
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        AuthorizationScope[] scopes = {new AuthorizationScope("global", "accessEverything")};
+        SecurityReference reference = new SecurityReference("JWT", scopes);
+        return SecurityContext.builder().securityReferences(Collections.singletonList(reference)).build();
+    }
+}
