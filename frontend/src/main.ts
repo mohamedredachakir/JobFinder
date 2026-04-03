799c25e90cfdbd20c180e8b3b0445112d1a2f364
reddcs <chakirm082@gmail.com>
2026-04-03 19:56:32 +0100

    feat: add frontend - src - main.ts

---
+++ frontend/src/main.ts
@@ -0,0 +1,15 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { appRoutes } from './app/app.routes';
+import { authInterceptor } from './app/core/interceptors/auth.interceptor';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimations(),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideRouter(appRoutes)
+  ]
+}).catch((err) => console.error(err));
