34e5492f9dec1678181fda8e206c81f57526a15d
reddcs <chakirm082@gmail.com>
2026-04-03 19:56:31 +0100

    feat: add frontend - src - app - app.component.spec.ts

---
+++ frontend/src/app/app.component.spec.ts
@@ -0,0 +1,10 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  it('should create app', async () => {
+    await TestBed.configureTestingModule({ imports: [AppComponent] }).compileComponents();
+    const fixture = TestBed.createComponent(AppComponent);
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+});
