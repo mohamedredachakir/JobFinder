5a47c8103a0cbb5ddad5c55bdf7659c0d4ba90a1
reddcs <chakirm082@gmail.com>
2026-04-03 19:56:31 +0100

    feat: add frontend - karma.conf.js

---
+++ frontend/karma.conf.js
@@ -0,0 +1,21 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: { jasmine: {} },
+    reporters: ['progress', 'coverage'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    restartOnFileChange: false
+  });
+};
